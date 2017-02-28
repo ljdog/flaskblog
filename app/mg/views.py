54b94661ee7ec769ad642d66dbeb45a6a0cbe130
guocq <guocqmail@163.com>
2017-02-28 16:31:51 +0800

    检查了一下 发现图片不能上传

--- app/mg/views.py
+++ app/mg/views.py
@@ -1,36 +1,43 @@
 # coding:utf-8
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, current_app
 from flask import request, flash, session
 from config import UPDATE_INFO
 from . import mg as mg_bp
 from .model import UploadForm
-from .. import set_mypic
+from app import set_mypic
 import app
 from app.share.helper_functions import login_required
 
+
 @mg_bp.route('/')
 def index():
-    rst = (list(app.mediaClass.get_all()))
-    return render_template('mg/index.html',rst=rst )
+    #rst = (list(app.mediaClass.get_all()))
+    rst = current_app.config.get('UPLOADED_MYPIC_DEST')
+    return render_template('mg/index.html', rst=rst)
+
 
 @mg_bp.route('/upload_img', methods=('GET', 'POST'))
-#@login_required()
+# @login_required()
 def upload_img():
     from datetime import datetime
     import time
     form = UploadForm()
     url = None
-   # app.logger.warn(u"进入函数")
+    # app.logger.warn(u"进入函数")
     url_list = []
     if form.validate_on_submit():
         # 因为每个图片要写上 图片说明 所有一次只能上传一张图片
         # for filename in request.files.getlist('upload'):
         # filename = form.upload.data.filename
+        print u"图片开始保存"
         str_folder = str(datetime.today())[:7]
         filename = form.upload.data.filename[:-4] + "_" + str(time.time())[:11]
-        url_list.append(set_mypic.url(set_mypic.save(form.upload.data, folder=str_folder, name=filename)))
+        print u"图片开始保存"
 
-        app.mediaClass.set_img_info(url_list[0], filename[:-1], request.form.get('explain'))
+        # url_list.append(set_mypic.url(set_mypic.save(form.upload.data, folder=str_folder, name=filename)))
+
+        print u"图片已经保存"
+        # app.mediaClass.set_img_info(url_list[0], filename[:-1], request.form.get('explain'))
 
     return render_template('mg/upload_img.html', form=form, url_list=url_list)
 
@@ -41,6 +48,7 @@ def get_img_info():
     rst_img_list = app.mediaClass.get_all()
     # app.logger.error(rst_img_list)
     return render_template('mg/img_info.html', rst_img_list=rst_img_list)
+
 
 @mg_bp.route('/add_user')
 @login_required()
@@ -82,7 +90,7 @@ def save_user():
     if not post_data['email'] or not post_data['_id']:
         flash('Username and Email are required..', 'error')
         if post_data['update']:
-                return redirect(url_for('.edit_user', id=post_data['_id']))
+            return redirect(url_for('.edit_user', id=post_data['_id']))
         else:
             return redirect(url_for('.add_user'))
     else:
