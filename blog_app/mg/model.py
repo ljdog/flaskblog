fa2a55c6239d0e26ec89770df9317a7cd1a33354
guocq <guocqmail@163.com>
2017-03-15 17:00:13 +0800

    修改一个导入的问

--- blog_app/mg/model.py
+++ blog_app/mg/model.py
@@ -3,7 +3,7 @@ from flask_wtf import Form
 from wtforms import SubmitField, StringField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-from app import set_mypic
+from blog_app.application import set_mypic
 
 
 class UploadForm(Form):
